beb2e89144591aba897cc1f60610cbf6957d4412
Sandeepa charith <sandeepacharith23@gmail.com>
2021-09-29 12:52:44 +0530

    Updated -What Focus On -List Builder with Json File Loading Data

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'colors.dart' as customcolors;
 
@@ -12,6 +15,19 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  
+  List info=[];
+  _initData(){
+    DefaultAssetBundle.of(context).loadString("json/info.json").then((value) {
+     info =json.decode(value);
+    });
+  }
+  
+  @override
+  void initState(){
+    super.initState();
+    _initData();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -227,6 +243,230 @@ class _HomepageState extends State<Homepage> {
                 ),
               ),
               ),
+              SizedBox(height: 10,),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 180,
+                
+
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(top: 30,),
+                      height: 120,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/card.jpg",
+                          ),
+                          fit: BoxFit.fill,
+                          ),
+
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 40,
+                              offset: Offset(8, 10),
+                              color: customcolors.AppColor.gradientSecond.withOpacity(0.8),
+                            ),
+
+                            BoxShadow(
+                              blurRadius: 40,
+                              offset: Offset(-1, -5),
+                              color: customcolors.AppColor.gradientSecond.withOpacity(0.8),
+                            ),
+
+                            
+                          ],
+                      ),
+                    ),
+                    Container(
+                      height: 200,
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(
+                      right: 200,
+                      bottom: 25,
+                      ),
+                      decoration: BoxDecoration(
+                        
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/figure.png",
+                          ),
+                          //fit: BoxFit.fill,
+                          ),
+
+                      ),
+                    
+
+                    ),
+                    Container(
+                      width: double.maxFinite,
+                      height: 100,
+                      //color: Colors.redAccent.withOpacity(0.2),
+                      margin: const EdgeInsets.only(
+                        left: 150,
+                        top: 30,
+                        ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          
+                          children: <Widget>
+                          [
+                            Text(
+                              "Your Progress is excellent",
+                              style: TextStyle(
+                                color: customcolors.AppColor.homePageDetail,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                
+                              ),
+                              ),
+                            SizedBox(height: 10,),
+                            RichText(
+                              text: TextSpan(
+                              text: "Keep it going \n",
+                              style: TextStyle(
+                                color: customcolors.AppColor.homePagePlanColor,
+                                fontSize: 16,
+
+                              ),
+                              children: [
+                                TextSpan(
+                              text: "Believe Your Self",
+                                ),
+                              ],
+                            ),
+                            ),
+                          
+                          ],
+                        ),
+                      ),
+                    ),                  
+                  ],
+                ),
+              ),
+              Row(
+                children: <Widget>
+                [
+                  Text(
+                    "Area of focus",
+                    style:TextStyle(
+                      fontSize:25,
+                      fontWeight: FontWeight.bold,
+                      color: customcolors.AppColor.homePageTitle,
+                    )
+                  ),
+                ],
+              ),
+
+              Expanded(
+              
+              child: OverflowBox(
+                maxWidth: MediaQuery.of(context).size.width,
+                child: ListView.builder
+                ( itemCount: (info.length.toDouble()/2).toInt() ,
+                  itemBuilder: (_,i)
+                  { 
+                    int a = 2*i;
+                    int b = 2*i +1;
+              
+                    return Row(
+                      children: <Widget>[
+                        Container(
+                          height: 170,
+                          width: (MediaQuery.of(context).size.width-60)/2,
+                          margin: EdgeInsets.only(left: 20,bottom: 15),
+                          padding: EdgeInsets.only(bottom: 5),
+                          decoration: BoxDecoration(
+                            color:Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            image: DecorationImage(
+                              image: AssetImage(
+                                info[a]['img'],
+                              ),
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  blurRadius: 3,
+                                  offset: Offset(5, 5),
+                                  color: customcolors.AppColor.gradientSecond.withOpacity(0.1),
+                                ),
+              
+                                BoxShadow(
+                                  blurRadius: 3,
+                                  offset: Offset(-5, -5),
+                                  color: customcolors.AppColor.gradientSecond.withOpacity(0.1),
+                                ),
+                              ],
+                          ),
+                          child: Center(
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                info[a]['title'],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: customcolors.AppColor.homePageDetail,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+              
+              
+                         Container(
+                          height: 170,
+                          width: (MediaQuery.of(context).size.width-60)/2,
+                          padding: EdgeInsets.only(bottom: 5),
+                          margin: EdgeInsets.only(left: 20,bottom: 15),
+                          decoration: BoxDecoration(
+                            color:Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            image: DecorationImage(
+                              image: AssetImage(
+                                info[b]['img'],
+                              ),
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  blurRadius: 3,
+                                  offset: Offset(5, 5),
+                                  color: customcolors.AppColor.gradientSecond.withOpacity(0.1),
+                                ),
+              
+                                BoxShadow(
+                                  blurRadius: 3,
+                                  offset: Offset(-5, -5),
+                                  color: customcolors.AppColor.gradientSecond.withOpacity(0.1),
+                                ),
+                              ],
+                          ),
+                          child: Center(
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                info[b]['title'],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: customcolors.AppColor.homePageDetail,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+              
+              
+                      ],
+                    );
+                    },
+                  ),
+              ),
+                ),
             ],
           ),
         ),
